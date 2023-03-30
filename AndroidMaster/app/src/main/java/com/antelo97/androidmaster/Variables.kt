c07efc4cc9c1e854e582ffbfd3d7c0c05d463132
Antelo97 <oscaran_97@hotmail.com>
2023-03-30 12:28:33 +0200

    Funciones incorporadas

--- AndroidMaster/app/src/main/java/com/antelo97/androidmaster/Variables.kt
+++ AndroidMaster/app/src/main/java/com/antelo97/androidmaster/Variables.kt
@@ -1,13 +1,31 @@
 package com.antelo97.androidmaster
 
+// Variable de clase, total o global
+val intExample1: Int = 30
+
 fun main() {
+    showMyName1()
+    showMyAge2(29)
+    showMyAgeByDefault()
+    add(25, 76)
+
+    val mySubtract: Int = subtract(10, 5)
+    println(mySubtract)
+
+    //variablesNumericas()
+    //variablesAlfanuméricas()
+    //variablesBoolean()
+}
+
+fun variablesNumericas() {
 
     /**
      * Variables numéricas
      */
 
+    println("VARIABLE NUMÉRICAS:")
+
     // Int [-2.147.483.647 a 2.147.483.647]
-    val intExample1: Int = 30
     var intExample2: Int = 30
     intExample2 = 29
 
@@ -21,7 +39,6 @@ fun main() {
     // Double (soporta hasta 14 decimales)
     val doubleExample: Double = 20.39483948239
 
-    /*
     println("Sumar:")
     println(intExample1 + intExample2)
 
@@ -36,7 +53,6 @@ fun main() {
 
     println("Módulo (resto):")
     println(intExample1 % intExample2)
-    */
 
     //println(intExample1 + longExample1)
     //println(intExample1 + floatExample)
@@ -44,9 +60,16 @@ fun main() {
     var exampleSuma1: Float = intExample1 + floatExample
     var exampleSuma2 = intExample1 + floatExample.toInt()
 
+    println()
+}
+
+fun variablesAlfanuméricas() {
+
     /**
      * Variables alfanuméricas
      */
+
+    println("VARIABLES ALFANUMÉRICAS:")
 
     // Char
     val charExample1: Char = 'a'
@@ -69,21 +92,55 @@ fun main() {
     stringConcatenada = "Hola me llamo $stringExample2 y tengo $intExample1 años"
     println(stringConcatenada)
 
-    val example123:String = intExample1.toString()
+    val example123: String = intExample1.toString()
     //val example123 = intExample1.toString()
+
+    println()
+}
+
+fun variablesBoolean() {
 
     /**
      * Variables booleanas
      */
+
+    println("VARIABLES BOOLEAN:")
 
     // Boolean
     var booleanExample1: Boolean = true
     var booleanExample2: Boolean = false
     var booleanExample3 = false
 
-    /*
-    println(stringExample1)
     println(booleanExample1)
-    println(doubleExample)
-    */
 }
+
+fun showMyName1() {
+    println("Me llamo Antelo")
+}
+
+fun showMyName2(name: String) {
+    println("Me llamo $name")
+}
+
+fun showMyAge1() {
+    println("Tengo 25 años")
+}
+
+fun showMyAge2(currentAge: Int) {
+    println("Tengo $currentAge años")
+}
+
+fun showMyAgeByDefault(currentAge: Int = 20) {
+    println("Tengo $currentAge años")
+}
+
+fun add(firstNumber: Int, secondNumber: Int) {
+    println(firstNumber + secondNumber)
+}
+
+fun subtract(firstNumber: Int, secondNumber: Int): Int {
+    return firstNumber - secondNumber
+}
+
+fun subtractCool1(firstNumber: Int, secondNumber: Int): Int = firstNumber - secondNumber
+fun subtractCool2(firstNumber: Int, secondNumber: Int) = firstNumber - secondNumber
