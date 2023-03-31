42c58ec6e14d484b10a25afc4f1827b3e50250da
Antelo97 <oscaran_97@hotmail.com>
2023-03-31 13:48:46 +0200

    Primer app finalizada

--- AndroidMaster/app/src/main/java/com/antelo97/androidmaster/firstapp/FirstAppActivity.kt
+++ AndroidMaster/app/src/main/java/com/antelo97/androidmaster/firstapp/FirstAppActivity.kt
@@ -1,11 +1,30 @@
 package com.antelo97.androidmaster.firstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import com.antelo97.androidmaster.R
 
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app)
+
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+        btnStart.setOnClickListener {
+            val name = etName.text.toString()
+
+            if (name.isNotEmpty()) {
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("EXTRA NAME", name)
+                startActivity(intent)
+            }
+        }
+        // Al arrancar la pantalla
     }
 }
