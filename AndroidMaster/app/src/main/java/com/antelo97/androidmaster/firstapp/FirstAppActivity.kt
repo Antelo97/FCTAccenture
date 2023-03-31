8deb6029e0b4620902113795e789742da16d9130
Antelo97 <oscaran_97@hotmail.com>
2023-03-31 13:45:52 +0200

    Primera app finalizada

---
+++ AndroidMaster/app/src/main/java/com/antelo97/androidmaster/firstapp/FirstAppActivity.kt
@@ -0,0 +1,11 @@
+package com.antelo97.androidmaster.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_app)
+    }
+}
