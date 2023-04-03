0960303bed3bb82e74df0ba791c502fe3ce5069d
Antelo97 <oscaran_97@hotmail.com>
2023-04-03 11:12:26 +0200

    Menú implementado

---
+++ AndroidMaster/app/src/main/java/com/antelo97/androidmaster/imccalculator/ImcCalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.antelo97.androidmaster.imccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.antelo97.androidmaster.R
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_calculator)
+    }
+}
