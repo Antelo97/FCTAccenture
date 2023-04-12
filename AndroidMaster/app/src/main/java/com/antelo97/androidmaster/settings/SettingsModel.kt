8fa09df4475f7978cd8d28835be1585a3ed35326
Antelo97 <oscaran_97@hotmail.com>
2023-04-12 14:26:43 +0200

    Pantalla de ajustes finalizada

---
+++ AndroidMaster/app/src/main/java/com/antelo97/androidmaster/settings/SettingsModel.kt
@@ -0,0 +1,8 @@
+package com.antelo97.androidmaster.settings
+
+data class SettingsModel(
+    var volume: Int,
+    var darkMode: Boolean,
+    var bluetooth: Boolean,
+    var vibration: Boolean
+)
