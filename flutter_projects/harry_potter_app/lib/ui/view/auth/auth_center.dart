fbaa9240c0db7ad21b553a48da5d44a4cc9d3bc1
Antelo97 <oscaran_97@hotmail.com>
2023-05-25 13:12:51 +0200

    Replantamiento de la estructura

---
+++ flutter_projects/harry_potter_app/lib/ui/view/auth/auth_center.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:harry_potter_app/helpers/loading/loading_screen.dart';
+import 'package:harry_potter_app/services/auth/firebase_auth_provider.dart';
+import 'package:harry_potter_app/ui/bloc/auth/auth_bloc.dart';
+import 'package:harry_potter_app/ui/bloc/auth/auth_event.dart';
+import 'package:harry_potter_app/ui/bloc/auth/auth_state.dart';
+
+import 'auth_center_views/forgot_password_view.dart';
+import 'auth_center_views/login_view.dart';
+import 'auth_center_views/register_view.dart';
+import 'auth_center_views/verify_email_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: BlocProvider<AuthBloc>(
+      create: (context) => AuthBloc(FirebaseAuthProvider()),
+      child: const HomePage(),
+    ),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    context.read<AuthBloc>().add(const AuthEventInitialize());
+    return BlocConsumer<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state.isLoading) {
+          LoadingScreen().show(
+            context: context,
+            text: state.loadingText ?? 'Plase wait a moment',
+          );
+        } else {
+          LoadingScreen().hide();
+        }
+      },
+      builder: (context, state) {
+        if (state is AuthStateLoggedIn) {
+          // return const NotesView();
+          return Container();
+        } else if (state is AuthStateNeedsVerification) {
+          return const VerifyEmailView();
+        } else if (state is AuthStateLoggedOut) {
+          return const LoginView();
+        } else if (state is AuthStateForgotPassword) {
+          return const ForgotPasswordView();
+        } else if (state is AuthStateRegistering) {
+          return const RegisterView();
+        } else {
+          return const Scaffold(
+            body: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+}
---
+++ flutter_projects/harry_potter_app/lib/utilities/dialogs/password_reset_email_sent_dialog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+import 'generic_dialog.dart';
+
+Future<void> showPasswordResentSentDialog(BuildContext context) {
+  return showGenericDialog<void>(
+    context: context,
+    title: 'Password Reset',
+    content:
+        'We have now sent you a password reset link. Please check your email for more information.',
+    optionsBuilder: () => {
+      'OK': null,
+    },
+  );
+}
